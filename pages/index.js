7346984f3211204b772c6a891f65c0cf52f8791e
Ryan Shea <ryan@shea.io>
2020-06-18 15:56:53 -0400

    add instructions to the home page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import { Container, Button, FormGroup, Input } from 'reactstrap'
 import fetch from 'isomorphic-unfetch'
-import { useQuerySettings } from '../lib/actions'
 import { connect } from 'react-redux'
 import useSWR from 'swr'
+import Link from 'next/link'
+import { useQuerySettings } from '../lib/actions'
 import { dbGraphFetcher } from '../lib/fetchers'
 
 const resolveUsers = async (db, twitterClient, group, limit, offset, count) => {
@@ -59,54 +60,79 @@ const Home = ({ database, twitterClient, twitterUsername }) => {
     dbGraphFetcher
 	)
 
+	const followersCount = followers && followers.count ? parseInt(followers.count) : 0
+	const followersResolvedCount = followers && followers.resolvedCount ? parseInt(followers.resolvedCount) : 0
+	const followeesCount = followees && followees.count ? parseInt(followees.count) : 0
+	const followeesResolvedCount = followees && followees.resolvedCount ? parseInt(followees.resolvedCount) : 0
+
 	return (
 		<>
 			<Container>
 				<h1 className="mt-5 text-center">Export Your Twitter Followers</h1>
 				<div className="mt-5 mb-5">
+					<h3>Instructions</h3>
+					<ol>
+						<li>
+							Go to the <Link href="/settings"><a>settings page</a></Link>
+						</li>
+						<li>
+							Create a Twitter app and enter in your Twitter API credentials
+						</li>
+						<li>
+							Create a postgresql database and enter your database settings
+						</li>
+						<li>
+							Return to the home page
+						</li>
+						<li>
+							Click the "Log Follower ID's" button to start logging your Twitter followers - don't close or refresh the page for at least a minute
+						</li>
+						<li>
+							Click the "Lookup Follower Profiles" button to start looking up the profiles of your Twitter followers - don't close or refresh the page for at least a minute
+						</li>
+						<li>
+							Repeat the previous two steps for your followees
+						</li>
+						<li>
+							View your followers and followees on their respective pages and click the "Message" button to send a direct message to a user or a group of users
+						</li>
+					</ol>
+				</div>
+				<div className="mt-5 mb-5">
 					<h3>Stats</h3>
 					<div className="card-columns">
-						{followers ? (
-							<>
-								<div className="card text-white bg-dark mb-3">
-									<div className="card-header">Follower ID's logged</div>
-									<div className="card-body">
-										<h4 className="card-title">
-											{parseInt(followers.count).toLocaleString()}
-										</h4>
-									</div>
-								</div>
-								<div className="card text-white bg-dark mb-3">
-									<div className="card-header">Follower profiles logged</div>
-									<div className="card-body">
-										<h4 className="card-title">
-											{parseInt(followers.resolvedCount).toLocaleString()}
-										</h4>
-									</div>
-								</div>
-							</>
-						) : null }
-
-						{followees ? (
-							<>
-								<div className="card text-white bg-dark mb-3">
-									<div className="card-header">Followee ID's logged</div>
-									<div className="card-body">
-										<h4 className="card-title">
-											{parseInt(followees.count).toLocaleString()}
-										</h4>
-									</div>
-								</div>
-								<div className="card text-white bg-dark mb-3">
-									<div className="card-header">Followee profiles logged</div>
-									<div className="card-body">
-										<h4 className="card-title">
-											{parseInt(followees.resolvedCount).toLocaleString()}
-										</h4>
-									</div>
-								</div>
-							</>
-						) : null }
+						<div className="card text-white bg-dark mb-3">
+							<div className="card-header">Follower ID's logged</div>
+							<div className="card-body">
+								<h4 className="card-title">
+									{followersCount.toLocaleString()}
+								</h4>
+							</div>
+						</div>
+						<div className="card text-white bg-dark mb-3">
+							<div className="card-header">Follower profiles logged</div>
+							<div className="card-body">
+								<h4 className="card-title">
+									{followeesCount.toLocaleString()}
+								</h4>
+							</div>
+						</div>
+						<div className="card text-white bg-dark mb-3">
+							<div className="card-header">Followee ID's logged</div>
+							<div className="card-body">
+								<h4 className="card-title">
+									{followersResolvedCount.toLocaleString()}
+								</h4>
+							</div>
+						</div>
+						<div className="card text-white bg-dark mb-3">
+							<div className="card-header">Followee profiles logged</div>
+							<div className="card-body">
+								<h4 className="card-title">
+									{followeesResolvedCount.toLocaleString()}
+								</h4>
+							</div>
+						</div>
 					</div>
 				</div>
 				<div className="mt-5 mb-5">
@@ -151,12 +177,12 @@ const Home = ({ database, twitterClient, twitterUsername }) => {
 						</Button>
 					</div>
 					<div className="mb-3 mt-3">
-						<FormGroup>
+						{/*<FormGroup>
 							<label>Limit</label>
 							<Input id="limit" value={querySettings.limit} placeholder="Limit"
 								onChange={(e) => setQuerySettings({ limit: parseInt(e.target.value) })}
 								type="number" />
-						</FormGroup>
+						</FormGroup>*/}
 						<FormGroup>
 							<label>Offset</label>
 							<Input id="offset" value={querySettings.offset} placeholder="Offset"
